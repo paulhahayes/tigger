8a186ca597e4212901bbf354bb61d3138ba2e1b4
Paul Hayes <100563530+paulhahayes@users.noreply.github.com>
2022-07-06 16:08:56 +1000

    Update tigger-add.sh

--- tigger-add.sh
+++ tigger-add.sh
@@ -1,22 +1,40 @@
 #!/bin/dash
 
-if [ -e .tigger ]; then
-    for file in $@
-    do
-    # check file name
-    if echo $file | grep -Ei '^[a-z0-9._-]*$' > /dev/null; then 
-        echo hello
-    else 
-        continue
-    fi
-
-    if [ ! -f "$file" ]; then
-        echo doesnt exist
-    fi
-    #check file exists
-    done
-    #metadata
-else 
-    exit 1
-
+if [ $# -lt 1 ]; then
+    >&2 echo "usage: tigger-add <filenames>"
 fi
+
+if  [ ! -d .tigger ];then
+    >&2 echo "tigger-add: error: tigger repository directory .tigger not found" # stder
+    exit 1
+fi
+
+
+for file in "$@"
+do
+# check file name
+if ! echo "$file" | grep -Ei '^[a-z0-9._-]*$' > /dev/null; then 
+    >&2 echo tigger-add: error: invalid filename "$file"
+    exit 1
+fi
+
+#check file exists
+if [ ! -f "$file" ]; then
+    >&2 echo tigger-add: error: can not open "'$file'"
+    exit
+fi
+
+# Verbose but safe way of getting the branch name of the current head 
+# could this be store as a env variable
+BRANCH="$(find .tigger/branches/head/ -mindepth 1 -maxdepth 1 -type d -exec basename {} \;)"
+COMMIT="$(find ./.tigger/branches/head/"$BRANCH"/ -type f  -printf "%f\n" | sort -r | head -1)"
+INDEXNAME=."$BRANCH"."$COMMIT"
+
+# check if the index exists
+if [ ! -d .tigger/index/"$INDEXNAME" ]; then
+    mkdir -p .tigger/index/"$INDEXNAME"
+fi
+cp "$file" .tigger/index/"$INDEXNAME"
+
+done
+
