f98883132051c0f9ccfa8221f7b496654e96e6b5
Paul Hayes <100563530+paulhahayes@users.noreply.github.com>
2022-07-08 08:45:13 +1000

    Update tigger-add.sh

--- tigger-add.sh
+++ tigger-add.sh
@@ -10,31 +10,34 @@ if  [ ! -d .tigger ];then
 fi
 
 
-for file in "$@"
-do
-# check file name
-if ! echo "$file" | grep -Ei '^[a-z0-9._-]*$' > /dev/null; then 
-    >&2 echo tigger-add: error: invalid filename "$file"
-    exit 1
-fi
-
-#check file exists
-if [ ! -f "$file" ]; then
-    >&2 echo tigger-add: error: can not open "'$file'"
-    exit
-fi
-
 # Verbose but safe way of getting the branch name of the current head 
-# could this be store as a env variable
 BRANCH="$(find .tigger/branches/head/ -mindepth 1 -maxdepth 1 -type d -exec basename {} \;)"
 COMMIT="$(find ./.tigger/branches/head/"$BRANCH"/ -type f  -printf "%f\n" | sort -r | head -1)"
-INDEXNAME=."$BRANCH"."$COMMIT"
+HEAD=."$BRANCH"."$COMMIT"
 
-# check if the index exists
-if [ ! -d .tigger/index/"$INDEXNAME" ]; then
-    mkdir -p .tigger/index/"$INDEXNAME"
-fi
-cp "$file" .tigger/index/"$INDEXNAME"
+for file in "$@"
+do
+    # check filename characters
+    if ! echo "$file" | grep -Ei '^[a-z0-9_.-]*$' > /dev/null; then 
+        >&2 echo "tigger-add: error: invalid filename '$file'"
+        exit 1
+    fi
 
+    # check file exists
+    if [ ! -f "$file" ] && [ ! -f .tigger/index/"$HEAD"/"$file" ]; then
+        >&2 echo "tigger-add: error: can not open '$file'"
+        exit
+    fi
+
+    # check if the index exists
+    if [ ! -d .tigger/index/"$HEAD" ]; then
+        mkdir -p .tigger/index/"$HEAD"
+    fi
+
+    if [ -f $file ];then 
+        cp "$file" .tigger/index/"$HEAD"
+    else 
+        rm -f .tigger/index/"$HEAD"/"$file"
+    fi
 done
 
